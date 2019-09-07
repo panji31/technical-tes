b95acf8edb4c4ae70db956790981109cb23d1b2c
panji31 <gumilarp@gmail.com>
2019-09-07 16:58:37 +0700

    soal no 3

---
+++ 3.php
@@ -0,0 +1,22 @@
+<?php
+function drawImage($jumlah){
+	for($i=1;$i<=$jumlah;$i++){
+		for($j=1;$j<=$jumlah;$j++){
+			if(($i==1&&$j==1) || ($i==1&&$j==$jumlah) || ($i==$jumlah&&$j==1) || ($i==$jumlah&&$j==$jumlah) ){
+				echo "*";
+			}else{
+				$tengah = ceil($jumlah/2);
+				if($i==$tengah || $j==$tengah){
+					echo "*";
+				}else{
+					echo "=";
+				}
+				
+			}
+			
+		}
+		echo "<br>";
+	}
+}
+drawImage(5);
+?>      
